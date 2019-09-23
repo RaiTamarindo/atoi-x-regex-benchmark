bb9baed7796e381bc451432b5a61ee5876c026b4
Raí Tamarindo <raitamarindo@gmail.com>
2019-09-23 14:50:41 -0300

    Improve benchmarking by splitting workload

--- main_test.go
+++ main_test.go
@@ -5,15 +5,61 @@ import (
 	"testing"
 )
 
-func BenchmarkAtoi(b *testing.B) {
+var workloads []string
+var worksize int
+
+func setup(success bool) {
+	if success {
+		workloads = []string{
+			"1",
+			"12",
+			"123",
+			"1234",
+			"12345",
+			"123456",
+			"1234567",
+			"12345678",
+			"123456789",
+			"1234567890",
+		}
+	} else {
+		workloads = []string{
+			"123456789o",
+			"abcdefghij",
+			"ABCDEFGHIJ",
+			"          ",
+			"!@#$%*()[]",
+		}
+	}
+	worksize = len(workloads)
+}
+
+func BenchmarkSuccessAtoi(b *testing.B) {
+	setup(true)
 	for i := 0; i < b.N; i++ {
-		validateWithAtoi("123456890")
+		validateWithAtoi(workloads[i%worksize])
 	}
 }
 
-func BenchmarkRegex(b *testing.B) {
+func BenchmarkSuccessRegex(b *testing.B) {
+	setup(true)
 	r := regexp.MustCompile("[0-9]+")
 	for i := 0; i < b.N; i++ {
-		validateWithRegex(r, "123456890")
+		validateWithRegex(r, workloads[i%worksize])
+	}
+}
+
+func BenchmarkFailAtoi(b *testing.B) {
+	setup(false)
+	for i := 0; i < b.N; i++ {
+		validateWithAtoi(workloads[i%worksize])
+	}
+}
+
+func BenchmarkFailRegex(b *testing.B) {
+	setup(false)
+	r := regexp.MustCompile("[0-9]+")
+	for i := 0; i < b.N; i++ {
+		validateWithRegex(r, workloads[i%worksize])
 	}
 }
